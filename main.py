f9a827617a54af19bf476001ec9fd1dc23ebfb84
Muhammad Farzad Ali <farzad0514@gmail.com>
2026-08-12 16:32:33 +0200

    chore: change default port to 8001

--- main.py
+++ main.py
@@ -3,4 +3,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("src.app:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("src.app:app", host="0.0.0.0", port=8001, reload=True)
